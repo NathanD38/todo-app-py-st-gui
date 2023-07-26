cb7d568fc48c5a96ac2bd2d12b96c8bdc1c8de4a
natha <nathan.dahan@gmail.com>
2023-07-26 13:33:56 +0300

    add complete case

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ user_prompt = "Enter a todo: "
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -11,12 +11,15 @@ while True:
             todo = input(user_prompt)
             todos.append(todo)
         case 'show':
-            for todo in todos:
-                print(todo)
+            for index, todo in enumerate(todos):
+                print(f'{index+1}-{todo}')
         case 'edit':
-            number = int(input('Which todo do you want to edit? '))-1
+            number = int(input('Which todo do you want to edit? ')) - 1
             new_todo = input("Edit: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input('Which todo do you want to complete? ')) - 1
+            todos.pop(number)
         case 'exit':
             break
 
