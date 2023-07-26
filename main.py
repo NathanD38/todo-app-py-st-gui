c45ec2cc7ea6ccaec77f0d913d97528913c3d9fe
natha <nathan.dahan@gmail.com>
2023-07-26 14:23:15 +0300

    implement ream/write operations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 user_prompt = "Enter a todo: "
-
-todos = []
+filepath = 'todos.txt'
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
@@ -9,14 +8,25 @@ while True:
     match user_action:
         case 'add':
             todo = input(user_prompt)
-            todos.append(todo)
+
+            with open(filepath) as file:
+                todos = file.readlines()
+
+            todos.append(todo+"\n")
+
+            with open(filepath, 'w') as file:
+                file.writelines(todos)
         case 'show':
+            with open(filepath) as file:
+                todos = file.readlines()
             for index, todo in enumerate(todos):
                 print(f'{index+1}-{todo}')
         case 'edit':
             number = int(input('Which todo do you want to edit? ')) - 1
             new_todo = input("Edit: ")
             todos[number] = new_todo
+            with open(filepath, 'a') as file:
+                file.writelines(new_todo)
         case 'complete':
             number = int(input('Which todo do you want to complete? ')) - 1
             todos.pop(number)
