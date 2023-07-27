073f767e51e688b852cac18bbed57b6ba20c83ff
natha <nathan.dahan@gmail.com>
2023-07-27 16:08:46 +0300

    Add buttons with functionality

--- main_gui.py
+++ main_gui.py
@@ -2,12 +2,30 @@ from functions import get_todos, write_todos
 import PySimpleGUI as psg
 
 label = psg.Text("Type n a To-do:")
-input_box = psg.InputText(tooltip='Enter a to-do item')
+input_box = psg.InputText(tooltip='Enter a to-do item', key='todo')
 add_button = psg.Button("Add")
 edit_button = psg.Button("Edit")
 complete_button = psg.Button("Complete")
 exit_button = psg.Button("Exit")
-app_gui = psg.Window(title='My To-Do App', layout=[[label], [input_box, add_button], [edit_button, complete_button], [exit_button]])
+app_gui = psg.Window(title='My To-Do App',
+                     layout=[[label],
+                             [input_box, add_button],
+                             [edit_button, complete_button],
+                             [exit_button]],
+                     font=('Helvetica', 15))
+while True:
+    event, values = app_gui.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            write_todos(todos)
+        case "Exit":
+            break
+        case psg.WIN_CLOSED:
+            break
 
-app_gui.read()
 app_gui.close()
