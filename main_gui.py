144b4160e56d1b12540f88fdd6d23ef4cfed7670
natha <nathan.dahan@gmail.com>
2023-07-28 17:25:31 +0300

    Final gui touch-ups

--- main_gui.py
+++ main_gui.py
@@ -1,11 +1,8 @@
 from functions import get_todos, write_todos
 import PySimpleGUI as psg
-# from datetime import datetime
-#
-# current_date = datetime.now()
-# formatted_date = current_date.strftime('It is %b %d, %Y %H:%M:%S')
-# # print(formatted_date)
+import time
 
+clock = psg.Text('', key='clock')
 label = psg.Text("Type in a To-do:")
 input_box = psg.InputText(tooltip='Enter a to-do item', key='todo')
 list_box = psg.Listbox(values=get_todos(), key='todos', enable_events=True, size=(40, 10))
@@ -14,16 +11,16 @@ edit_button = psg.Button("Edit")
 complete_button = psg.Button("Complete")
 exit_button = psg.Button("Exit")
 app_gui = psg.Window(title='My To-Do App',
-                     layout=[[label],
+                     layout=[[clock],
+                             [label],
                              [input_box, add_button],
-                             [list_box, edit_button],
-                             [complete_button, exit_button]
+                             [list_box, edit_button, complete_button],
+                             [exit_button]
                              ],
                      font=('Helvetica', 15))
 while True:
-    event, values = app_gui.read()
-    print(event)
-    print(values)
+    event, values = app_gui.read(timeout=200)
+    app_gui['clock'].update(value=time.strftime('%b %d, %Y %H:%M:%S'))
     match event:
         case "Add":
             if values['todo'] != '':
@@ -59,6 +56,8 @@ while True:
                 write_todos(todos)
                 app_gui['todos'].update(values=todos)
                 app_gui['todo'].update(value='')
+            except ValueError:
+                psg.Popup('Item is not in the to-do list!', title='Error', font=('Helvetica', 15), auto_close=True, auto_close_duration=5)
             except IndexError:
                 psg.Popup('Please select an item first!', title='Error', font=('Helvetica', 15), auto_close=True, auto_close_duration=5)
 
